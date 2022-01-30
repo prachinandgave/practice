857287740767a62acdc94f3d9ad3054b7795bc96
prachinandgave <nandgaveprachi@gmail.com>
2022-01-30 19:46:22 +0530

    aadding

---
+++ A basic programs/src/E1.java
@@ -0,0 +1,21 @@
+
+public class E1{
+	String a="priya";
+	int b=26;
+	float c=5.5f;
+	String d="navezari,post navezari,tah tirora";
+	void studentinformation() {
+		System.out.println(" student name:"+a);
+				
+		System.out.println("age:"+b);
+		System.out.println("hight:"+c);
+		System.out.println("address,\n"+d);
+	}
+		
+		public static void main(String[] args) {
+			E1 hp=new E1();
+			hp.studentinformation();
+		
+	}
+
+}
